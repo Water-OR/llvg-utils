6235b3f37a381f4a51097e765e045434b061e934
Water-OR <a12300680@126.com>
2025-04-09 20:06:45 +0800

    feat: improve iterator.ArrayHelper

--- lib/src/main/java/net/llvg/loliutils/iterator/ArrayHelper.java
+++ lib/src/main/java/net/llvg/loliutils/iterator/ArrayHelper.java
@@ -19,11 +19,35 @@
 
 package net.llvg.loliutils.iterator;
 
+import net.llvg.loliutils.exception.TypeCast;
+
 import java.lang.reflect.Array;
 
-public class ArrayHelper {
-        @SuppressWarnings ("unchecked")
+public final class ArrayHelper {
+        private ArrayHelper() { }
+        
         public static <T> T[] array(Class<? extends T> type, int size) {
-                return (T[]) Array.newInstance(type, size);
+                return TypeCast.cast(Array.newInstance(type, size));
+        }
+        
+        public static void checkRangeTill(int size, int till) {
+                if (till < 0) {
+                        throw new IllegalArgumentException("till(" + till + ") < 0");
+                }
+                if (till > size) {
+                        throw new IllegalArgumentException("till(" + till + ") > size(" + size + ")");
+                }
+        }
+        
+        public static void checkRangeFromTill(int size, int from, int till) {
+                if (from < 0) {
+                        throw new IllegalArgumentException("from(" + from + ") < 0");
+                }
+                if (till < from) {
+                        throw new IllegalArgumentException("till(" + till + ") < from(" + from + ")");
+                }
+                if (till > size) {
+                        throw new IllegalArgumentException("till(" + till + ") > size(" + size + ")");
+                }
         }
 }
