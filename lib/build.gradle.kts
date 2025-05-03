2cc9001536d15f562152c4b1591880663f8e2a41
Water-OR <a12300680@126.com>
2025-05-03 10:39:15 +0800

    bump: update buildscript

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-        kotlin("jvm") version libs.versions.kotlin
-        
-        `java-library`
-        `maven-publish`
-        publishing
+    kotlin("jvm") version libs.versions.kotlin
+    
+    `java-library`
+    `maven-publish`
+    publishing
 }
 
 @Suppress("PropertyName")
@@ -13,51 +13,52 @@ group = "net.llvg"
 version = project_version
 
 repositories {
-        mavenCentral()
+    mavenCentral()
 }
 
 @Suppress("UnstableApiUsage")
 testing.suites {
-        val test by getting(JvmTestSuite::class) {
-                useKotlinTest(libs.versions.kotlin)
-        }
+    val test by getting(JvmTestSuite::class) {
+        useKotlinTest(libs.versions.kotlin)
+    }
 }
 
 java {
-        toolchain {
-                languageVersion = JavaLanguageVersion.of(8)
-                vendor = JvmVendorSpec.AZUL
-        }
-        
-        withSourcesJar()
-        withJavadocJar()
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(8)
+        vendor = JvmVendorSpec.AZUL
+    }
+    
+    withSourcesJar()
+    withJavadocJar()
 }
 
 kotlin {
-        compilerOptions {
-                val args = listOf(
-                        "-Xjvm-default=all"
-                )
-                freeCompilerArgs.addAll(args)
-        }
+    compilerOptions {
+        val args = listOf(
+            "-Xjvm-default=all",
+            "-opt-in=kotlin.contracts.ExperimentalContracts"
+        )
+        freeCompilerArgs.addAll(args)
+    }
 }
 
 publishing {
-        repositories {
-                mavenLocal()
+    repositories {
+        mavenLocal()
+    }
+    
+    publications {
+        val maven by registering(MavenPublication::class) {
+            from(components["kotlin"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+            pom {
+                artifactId = "loli-utils"
+                name = "loli-utils"
+                description = "LolI Vanguard's utils <3"
+                url = "https://github.com/Water-OR/llvg-utils"
+            }
         }
-        
-        publications {
-                val maven by registering(MavenPublication::class) {
-                        from(components["kotlin"])
-                        artifact(tasks["sourcesJar"])
-                        artifact(tasks["javadocJar"])
-                        pom {
-                                artifactId = "loli-utils"
-                                name = "loli-utils"
-                                description = "LolI Vanguard's utils <3"
-                                url = "https://github.com/Water-OR/llvg-utils"
-                        }
-                }
-        }
+    }
 }
