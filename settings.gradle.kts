8f1a80271f419cadbc15fabc08bcad76871c46f0
Water-OR <a12300680@126.com>
2025-05-08 17:23:48 +0800

    bump: upgrade plugin org.gradle.toolchains.foojay-resolver-convention to version 0.10.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 @file:Suppress("SpellCheckingInspection")
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
 }
 
 rootProject.name = "LLVG Utils"
