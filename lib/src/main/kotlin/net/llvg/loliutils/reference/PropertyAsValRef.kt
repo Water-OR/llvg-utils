3eb5f1aaf25ed3248b4ba846108116b2f00ac83b
Water-OR <a12300680@126.com>
2025-05-10 12:13:42 +0800

    correct license

--- lib/src/main/kotlin/net/llvg/loliutils/reference/PropertyAsValRef.kt
+++ lib/src/main/kotlin/net/llvg/loliutils/reference/PropertyAsValRef.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2025 Water-OR
  *
- * This file is part of LolI Utils
+ * This file is part of llvg-utils
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
