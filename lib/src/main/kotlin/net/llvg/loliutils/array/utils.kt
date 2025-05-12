792cdcabd55cd13e52da69a7fdfda546dd2e1760
Water-OR <a12300680@126.com>
2025-05-13 00:06:14 +0800

    refactor: rewrite array utils

--- lib/src/main/kotlin/net/llvg/loliutils/array/utils.kt
+++ lib/src/main/kotlin/net/llvg/loliutils/array/utils.kt
@@ -18,16 +18,17 @@
  */
 
 @file:JvmName("ArrayUtils")
-@file:Suppress("ReplaceJavaStaticMethodWithKotlinAnalog")
 
 package net.llvg.loliutils.array
 
 import java.util.Arrays
+import kotlin.internal.Exact
 import kotlin.internal.InlineOnly
 import kotlin.internal.PureReifiable
+import net.llvg.loliutils.type.cast
 
 @InlineOnly
-public inline fun <T> newArray(
+public inline fun <T> newTypedArray(
     type: Class<T>,
     size: Int
 ): Array<T> =
@@ -37,10 +38,10 @@ public inline fun <T> newArray(
     )
 
 @InlineOnly
-public inline fun <@PureReifiable reified T> newArray(
+public inline fun <@PureReifiable reified T> newTypedArray(
     size: Int
-): Array<T> =
-    newArray(
+): Array<@Exact T> =
+    newTypedArray(
         T::class.java,
         size
     )
@@ -51,132 +52,261 @@ public inline val <T> Array<T>.asList: MutableList<T>
     get() = Arrays.asList(*this)
 
 @InlineOnly
+public inline fun ByteArray.copyTo(
+    dest: ByteArray,
+    destOffset: Int = 0,
+    from: Int = 0,
+    length: Int = size - from
+): ByteArray {
+    System.arraycopy(this, from, dest, destOffset, length)
+    return dest
+}
+
+@InlineOnly
+public inline fun ShortArray.copyTo(
+    dest: ShortArray,
+    destOffset: Int = 0,
+    from: Int = 0,
+    length: Int = size - from
+): ShortArray {
+    System.arraycopy(this, from, dest, destOffset, length)
+    return dest
+}
+
+@InlineOnly
+public inline fun IntArray.copyTo(
+    dest: IntArray,
+    destOffset: Int = 0,
+    from: Int = 0,
+    length: Int = size - from
+): IntArray {
+    System.arraycopy(this, from, dest, destOffset, length)
+    return dest
+}
+
+@InlineOnly
+public inline fun LongArray.copyTo(
+    dest: LongArray,
+    destOffset: Int = 0,
+    from: Int = 0,
+    length: Int = size - from
+): LongArray {
+    System.arraycopy(this, from, dest, destOffset, length)
+    return dest
+}
+
+@InlineOnly
+public inline fun CharArray.copyTo(
+    dest: CharArray,
+    destOffset: Int = 0,
+    from: Int = 0,
+    length: Int = size - from
+): CharArray {
+    System.arraycopy(this, from, dest, destOffset, length)
+    return dest
+}
+
+@InlineOnly
+public inline fun FloatArray.copyTo(
+    dest: FloatArray,
+    destOffset: Int = 0,
+    from: Int = 0,
+    length: Int = size - from
+): FloatArray {
+    System.arraycopy(this, from, dest, destOffset, length)
+    return dest
+}
+
+@InlineOnly
+public inline fun DoubleArray.copyTo(
+    dest: DoubleArray,
+    destOffset: Int = 0,
+    from: Int = 0,
+    length: Int = size - from
+): DoubleArray {
+    System.arraycopy(this, from, dest, destOffset, length)
+    return dest
+}
+
+@InlineOnly
+public inline fun BooleanArray.copyTo(
+    dest: BooleanArray,
+    destOffset: Int = 0,
+    from: Int = 0,
+    length: Int = size - from
+): BooleanArray {
+    System.arraycopy(this, from, dest, destOffset, length)
+    return dest
+}
+
+@InlineOnly
+public inline fun <@PureReifiable T> Array<out T>.copyTo(
+    dest: Array<@Exact T>,
+    destOffset: Int = 0,
+    from: Int = 0,
+    length: Int = size - from
+): Array<@Exact T> {
+    System.arraycopy(this, from, dest, destOffset, length)
+    return dest
+}
+
 @PublishedApi
-internal fun subArrayRangeCheck(
-    from: Int,
-    till: Int,
-    size: Int
-): Int {
-    require(0 <= from) { "Failed in check 0 <= from, from=$from" }
-    require(from < till) { "Failed in check from < till, from=$from, till=$till" }
-    require(till < size) { "Failed in check till < size, till=$till, size=$size" }
-    
-    return till
+internal fun subArrayBeginIndexCheck(
+    beginIndex: Int,
+    sourceSize: Int
+) {
+    require(beginIndex >= 0) {
+        "beginIndex($beginIndex) must be greater than or equals to 0"
+    }
+    require(beginIndex < sourceSize) {
+        "beginIndex($beginIndex) must be less than sourceSize($sourceSize)"
+    }
+}
+
+@PublishedApi
+internal fun subArrayEndIndexCheck(
+    endIndex: Int,
+    sourceSize: Int
+) {
+    require(endIndex <= sourceSize) {
+        "beginIndex+length=$endIndex must be less than or equal to sourceSize=$sourceSize"
+    }
 }
 
 @InlineOnly
 public inline fun ByteArray.subArray(
-    from: Int = 0,
-    size: Int = this.size - from
-): ByteArray =
-    subArrayRangeCheck(from, from + size, this.size).let { till ->
-        if (from == 0) {
-            Arrays.copyOf(this, size)
-        } else {
-            Arrays.copyOfRange(this, from, till)
-        }
-    }
+    beginIndex: Int = 0,
+    length: Int = size - beginIndex
+): ByteArray {
+    subArrayBeginIndexCheck(beginIndex, size)
+    
+    if (length == 0) return ByteArray(0)
+    subArrayEndIndexCheck(beginIndex + length, size)
+    
+    return copyTo(ByteArray(length), 0, beginIndex, length)
+}
 
 @InlineOnly
 public inline fun ShortArray.subArray(
-    from: Int = 0,
-    size: Int = this.size - from
-): ShortArray =
-    subArrayRangeCheck(from, from + size, this.size).let { till ->
-        if (from == 0) {
-            Arrays.copyOf(this, size)
-        } else {
-            Arrays.copyOfRange(this, from, till)
-        }
-    }
+    beginIndex: Int = 0,
+    length: Int = size - beginIndex
+): ShortArray {
+    subArrayBeginIndexCheck(beginIndex, size)
+    
+    if (length == 0) return ShortArray(0)
+    subArrayEndIndexCheck(beginIndex + length, size)
+    
+    return copyTo(ShortArray(length), 0, beginIndex, length)
+}
 
 @InlineOnly
 public inline fun IntArray.subArray(
-    from: Int = 0,
-    size: Int = this.size - from
-): IntArray =
-    subArrayRangeCheck(from, from + size, this.size).let { till ->
-        if (from == 0) {
-            Arrays.copyOf(this, size)
-        } else {
-            Arrays.copyOfRange(this, from, till)
-        }
-    }
+    beginIndex: Int = 0,
+    length: Int = size - beginIndex
+): IntArray {
+    subArrayBeginIndexCheck(beginIndex, size)
+    
+    if (length == 0) return IntArray(0)
+    subArrayEndIndexCheck(beginIndex + length, size)
+    
+    return copyTo(IntArray(length), 0, beginIndex, length)
+}
 
 @InlineOnly
 public inline fun LongArray.subArray(
-    from: Int = 0,
-    size: Int = this.size - from
-): LongArray =
-    subArrayRangeCheck(from, from + size, this.size).let { till ->
-        if (from == 0) {
-            Arrays.copyOf(this, size)
-        } else {
-            Arrays.copyOfRange(this, from, till)
-        }
-    }
+    beginIndex: Int = 0,
+    length: Int = size - beginIndex
+): LongArray {
+    subArrayBeginIndexCheck(beginIndex, size)
+    
+    if (length == 0) return LongArray(0)
+    subArrayEndIndexCheck(beginIndex + length, size)
+    
+    return copyTo(LongArray(length), 0, beginIndex, length)
+}
 
 @InlineOnly
 public inline fun CharArray.subArray(
-    from: Int = 0,
-    size: Int = this.size - from
-): CharArray =
-    subArrayRangeCheck(from, from + size, this.size).let { till ->
-        if (from == 0) {
-            Arrays.copyOf(this, size)
-        } else {
-            Arrays.copyOfRange(this, from, till)
-        }
-    }
+    beginIndex: Int = 0,
+    length: Int = size - beginIndex
+): CharArray {
+    subArrayBeginIndexCheck(beginIndex, size)
+    
+    if (length == 0) return CharArray(0)
+    subArrayEndIndexCheck(beginIndex + length, size)
+    
+    return copyTo(CharArray(length), 0, beginIndex, length)
+}
 
 @InlineOnly
 public inline fun FloatArray.subArray(
-    from: Int = 0,
-    size: Int = this.size - from
-): FloatArray =
-    subArrayRangeCheck(from, from + size, this.size).let { till ->
-        if (from == 0) {
-            Arrays.copyOf(this, size)
-        } else {
-            Arrays.copyOfRange(this, from, till)
-        }
-    }
+    beginIndex: Int = 0,
+    length: Int = size - beginIndex
+): FloatArray {
+    subArrayBeginIndexCheck(beginIndex, size)
+    
+    if (length == 0) return FloatArray(0)
+    subArrayEndIndexCheck(beginIndex + length, size)
+    
+    return copyTo(FloatArray(length), 0, beginIndex, length)
+}
 
 @InlineOnly
 public inline fun DoubleArray.subArray(
-    from: Int = 0,
-    size: Int = this.size - from
-): DoubleArray =
-    subArrayRangeCheck(from, from + size, this.size).let { till ->
-        if (from == 0) {
-            Arrays.copyOf(this, size)
-        } else {
-            Arrays.copyOfRange(this, from, till)
-        }
-    }
+    beginIndex: Int = 0,
+    length: Int = size - beginIndex
+): DoubleArray {
+    subArrayBeginIndexCheck(beginIndex, size)
+    
+    if (length == 0) return DoubleArray(0)
+    subArrayEndIndexCheck(beginIndex + length, size)
+    
+    return copyTo(DoubleArray(length), 0, beginIndex, length)
+}
 
 @InlineOnly
 public inline fun BooleanArray.subArray(
-    from: Int = 0,
-    size: Int = this.size - from
-): BooleanArray =
-    subArrayRangeCheck(from, from + size, this.size).let { till ->
-        if (from == 0) {
-            Arrays.copyOf(this, size)
-        } else {
-            Arrays.copyOfRange(this, from, till)
-        }
-    }
+    beginIndex: Int = 0,
+    length: Int = size - beginIndex
+): BooleanArray {
+    subArrayBeginIndexCheck(beginIndex, size)
+    
+    if (length == 0) return BooleanArray(0)
+    subArrayEndIndexCheck(beginIndex + length, size)
+    
+    return copyTo(BooleanArray(length), 0, beginIndex, length)
+}
 
 @InlineOnly
-public inline fun <T> Array<T>.subArray(
-    from: Int = 0,
-    size: Int = this.size - from
+public inline fun <@PureReifiable reified T> Array<out T>.subArray(
+    beginIndex: Int = 0,
+    length: Int = size - beginIndex
+): Array<@Exact T> {
+    subArrayBeginIndexCheck(beginIndex, size)
+    
+    if (length == 0) return newTypedArray(0)
+    subArrayEndIndexCheck(beginIndex + length, size)
+    
+    return copyTo(newTypedArray<@Exact T>(length), 0, beginIndex, length)
+}
+
+@InlineOnly
+public inline fun <T> Array<out T>.subArray(
+    targetType: Class<T>,
+    beginIndex: Int = 0,
+    length: Int = size - beginIndex
+): Array<T> {
+    subArrayBeginIndexCheck(beginIndex, size)
+    
+    if (length == 0) return newTypedArray(targetType, 0)
+    subArrayEndIndexCheck(beginIndex + length, size)
+    
+    return copyTo(newTypedArray(targetType, length), 0, beginIndex, length)
+}
+
+@InlineOnly
+public inline fun <T> Array<T>.subArrayUnsafe(
+    targetFrom: Int = 0,
+    length: Int = size - targetFrom
 ): Array<T> =
-    subArrayRangeCheck(from, from + size, this.size).let { till ->
-        if (from == 0) {
-            Arrays.copyOf(this, size)
-        } else {
-            Arrays.copyOfRange(this, from, till)
-        }
-    }
+    subArray(cast<Class<T>>(javaClass.componentType), targetFrom, length)
